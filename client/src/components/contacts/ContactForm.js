65a31a909ff07cfe288e5e8fa36c0aca1ef90b0c
Asher <asher_3@hotmail.co.uk>
2020-09-13 20:27:57 +0100

    have a bug in my code in trying to add functionality for editing & updating a specific contact. Working on discovering the issue

--- client/src/components/contacts/ContactForm.js
+++ client/src/components/contacts/ContactForm.js
@@ -1,8 +1,24 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import ContactContext from "../../context/contact/contactContext";
 
 const ContactForm = () => {
   const contactContext = useContext(ContactContext);
+
+  const { updateContact, addContact, clearCurrent, current } = contactContext;
+
+  useEffect(() => {
+    if (current !== null) {
+      setContact(current);
+    } else {
+      setContact({
+        name: "",
+        email: "",
+        phone: "",
+        type: "personal",
+      });
+    }
+  }, [contactContext, current]);
+
   const [contact, setContact] = useState({
     name: "",
     email: "",
@@ -16,7 +32,11 @@ const ContactForm = () => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    contactContext.addContact(contact);
+    if (current === null) {
+      addContact(contact);
+    } else {
+      updateContact(contact);
+    }
     setContact({
       name: "",
       email: "",
@@ -25,9 +45,13 @@ const ContactForm = () => {
     });
   };
 
+  const clearAll = () => {
+    clearCurrent();
+  };
+
   return (
     <form onSubmit={onSubmit}>
-      <h2 className="text-primary">Add Contact</h2>
+      <h2 className="text-primary">{current ? "Edit Contact" : "Add Contact"}</h2>
       <input type="text" placeholder="Name" name="name" value={name} onChange={onChange} />
       <input type="email" placeholder="Email" name="email" value={email} onChange={onChange} />
       <input type="text" placeholder="Phone" name="phone" value={phone} onChange={onChange} />
@@ -49,8 +73,19 @@ const ContactForm = () => {
       />{" "}
       Professional
       <div>
-        <input type="submit" value="Add Contact" className="btn btn-primary btn-block" />
+        <input
+          type="submit"
+          value={current ? "Update Contact" : "Add Contact"}
+          className="btn btn-primary btn-block"
+        />
       </div>
+      {current && (
+        <div>
+          <button className="btn btn-light btn-block" onClick={clearAll}>
+            Clear
+          </button>
+        </div>
+      )}
     </form>
   );
 };
