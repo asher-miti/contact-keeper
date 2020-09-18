2c1442869e74d770da794f0b1a6aac13758d621f
Asher <asher_3@hotmail.co.uk>
2020-09-18 18:00:12 +0100

    created alerts and components for user registration using hooks in Register.js file

--- client/src/context/alert/alertReducer.js
+++ client/src/context/alert/alertReducer.js
@@ -3,8 +3,7 @@ import { SET_ALERT, REMOVE_ALERT } from "../types";
 export default (state, action) => {
   switch (action.type) {
     case SET_ALERT:
-      return;
-      [...state, action.payload];
+      return [...state, action.payload];
     case REMOVE_ALERT:
       return state.filter((alert) => alert.id !== action.payload);
     default:
